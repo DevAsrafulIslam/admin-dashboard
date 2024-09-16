31846d2cc73eb74f34ded2891079601937df0284
MD Asraful Islam <asrafulislam.dev@gmail.com>
2024-09-17 02:44:14 +0600

    added another pages

---
+++ src/app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import BackButton from "@/components/BackButton";
+import { Form } from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+  body: z.string().min(1, {
+    message: "Body is required",
+  }),
+  author: z.string().min(1, {
+    message: "Author is required",
+  }),
+  date: z.string().min(1, {
+    message: "Date is required",
+  }),
+});
+interface PostEditPageProps {
+  params: {
+    id: string;
+  };
+}
+const PostEditPage = ({ params }: PostEditPageProps) => {
+  const post = posts.find((post) => post.id === params(params.id));
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: post?.title || "",
+      body: post?.body || "",
+      author: post?.author || "",
+      date: post?.date || "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <BackButton text="Back to Post" link="/posts" />
+      <h3 className="text-2xl mb-4">Edit Post</h3>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default PostEditPage;
